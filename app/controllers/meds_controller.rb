55a78ae56ffa0f9bf6078ce15612e160d7dc4b51
Luis Gabriel Dayeh <luisgabriel.dayeh@gmail.com>
2021-09-07 15:17:26 +0200

    added routes and actions to new meds

--- app/controllers/meds_controller.rb
+++ app/controllers/meds_controller.rb
@@ -1,7 +1,34 @@
 class MedsController < ApplicationController
-  def show
-  end
 
   def index
+    @meds = policy_scope(Med).order(created_at: :desc)
+  end
+
+  def show
+    @med = Med.find(params[:id])
+    authorize @med
+  end
+
+  def new
+    @med = Med.new
+    authorize @med
+  end
+
+  def create
+    @med = Med.new(med_params)
+    authorize @med
+    @med.user = current_user
+    @med.save
+    if @med.save
+      redirect_to med_path(@med)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def med_params
+    params.require(:med).permit(:name, :description, :stock)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'meds/show'
-  get 'meds/index'
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#landing_page'
+  resources :pages, only: [:landing_page] do
+    collection do
+      get :dashboard
+    end
+  end
+  resources :meds, only: [ :new, :create, :index, :show ]
 end
