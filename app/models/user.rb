7dcb4dc2888ae6a1e5da7a63ebc62fdf0f1aa1c5
Samuel <samueltitterington@gmail.com>
2021-09-08 12:09:40 +0200

    Fixed pundit, added validations and added a default to stock (0)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :meds, dependent: :destroy
   has_many :treatments, dependent: :destroy
   has_many :contacts, dependent: :destroy
   has_many :schedules, through: :meds
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
