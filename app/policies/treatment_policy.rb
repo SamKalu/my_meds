eb88dfba71097ecec958f516e3416feaba679e96
sami janafse <goblinsami@gmail.com>
2021-09-08 17:46:47 +0200

    treatments back end and front end

---
+++ app/policies/treatment_policy.rb
@@ -0,0 +1,35 @@
+class TreatmentPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#landing_page'
   get "/dashboard", to: "pages#dashboard"
   resources :meds
+  resources :treatments, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 end
