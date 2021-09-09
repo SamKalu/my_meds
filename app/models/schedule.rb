f14bf4c8a780e8921e595005f4e31c8498a8f61e
Samuel <samueltitterington@gmail.com>
2021-09-09 14:54:47 +0200

    Built weekdays and active inactive button

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,12 @@ class Schedule < ApplicationRecord
   enum status: %i[inactive active]
   belongs_to :med
   belongs_to :treatment
+
+  WEEKDAYS = [['MO', 'monday'],
+              ['TU', 'tuesday'],
+              ['WE', 'wednesday'],
+              ['TH', 'thursday'],
+              ['FR', 'friday'],
+              ['SA', 'saturday'],
+              ['SU', 'sunday']]
 end
