69b96ffad29769935c26f26d045f0159c682f62b
Luis Gabriel Dayeh <luisgabriel.dayeh@gmail.com>
2021-09-09 16:45:17 +0200

    created the hours for schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,9 @@ class Schedule < ApplicationRecord
   enum status: %i[inactive active]
   belongs_to :med
   belongs_to :treatment
+  before_validation :clean_arrays
+
+  def clean_arrays
+    self.times = times.reject(&:empty?)
+  end
 end
