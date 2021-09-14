29a3ba1bd27aba5bdc064dedc6ac4551d0240643
Samuel <samueltitterington@gmail.com>
2021-09-14 17:26:20 +0200

    I did it bois, it actually works and its beautiful :')

--- app/javascript/controllers/display_cards_controller.js
+++ app/javascript/controllers/display_cards_controller.js
@@ -4,17 +4,22 @@ export default class extends Controller {
   static targets = [ "trigger", "output" ]
 
   connect() {
-    console.log(this.triggerTargets)
-    console.log(this.outputTargets)
   }
   show(event) {
-    this.triggerTargets.forEach( (el) => el.classList.remove("treatment-card-selected"))
-    const selectedCard = event.currentTarget
-    console.log(selectedCard)
-    selectedCard.classList.add("treatment-card-selected")
-
-    this.outputTargets.forEach( (el) => el.classList.add("d-none"))
     const elementToShow = this.outputTargets.find( (el) => el.id === event.currentTarget.id )
-    elementToShow.classList.remove("d-none")
+
+    if (event.currentTarget.classList.contains("treatment-card-selected")) {
+      event.currentTarget.classList.remove("treatment-card-selected");
+      elementToShow.classList.add("d-none");
+    } else {
+      this.triggerTargets.forEach( (el) => el.classList.remove("treatment-card-selected"))
+      const selectedCard = event.currentTarget
+      console.log(selectedCard)
+      selectedCard.classList.add("treatment-card-selected")
+
+      this.outputTargets.forEach( (el) => el.classList.add("d-none"))
+      elementToShow.classList.remove("d-none")
+    }
+    
   }
 }
