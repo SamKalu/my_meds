70671011e5177561c8a7661602249a0c553e62bf
Luis Gabriel Dayeh <luisgabriel.dayeh@gmail.com>
2021-09-16 15:16:42 +0200

    alerts for schedules are now js

--- app/javascript/controllers/intakes_taken_controller.js
+++ app/javascript/controllers/intakes_taken_controller.js
@@ -13,15 +13,17 @@ export default class extends Controller {
       headers: { 'X-CSRF-Token': csrfToken() }
     })
       .then(response => {
-        const btnHtml = `<i class="fas fa-check-circle mr-2"></i><p>Med taken</p>`
+        const btnHtml = `<i class="fas fa-check-circle mr-2"></i><p>Med taken</p>`;
+        const dayTime = this.iconTarget.dataset.name;
+        const alert = document.querySelector(`#${dayTime}-alert`);
         if (response.status == 200) {
           this.medsTargets.find(x => x.id === `intake-${intakeId}`).classList.toggle("taken");
           this.btnTargets.find(x => x.dataset.id === `${intakeId}`).innerHTML = btnHtml;
           console.log(this.btnTargets)
           console.log(this.btnTargets.every(x => x.querySelector(".fa-check-circle")))
           if (this.btnTargets.every(x => x.querySelector(".fa-check-circle"))) {
-            console.log(this.iconTargets)
             this.iconTargets[0].innerHTML = '<i class="fas fa-check-circle"></i>'
+            alert.classList.add("d-none")
           }
         }
       })
